fb11a911a0e828d0cf7f7ecd1ea64d8130f46e98
Lyle-Anthony Aigbedion <laigbedion@bloomberg.net>
2022-07-26 14:59:46 -0400

    Modify tsconfig and jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-};
+  transformIgnorePatterns: [
+    '/node_modules/(?!graphviz-react)'
+  ],
+  transform: {
+'^.+\\.(ts|tsx|js)$': 'ts-jest'
+  }
+}
